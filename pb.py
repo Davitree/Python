8ba10ea9be597b28523e66bb97c103bc4c3a7be7
Davitree <davidkhadka444@gmail.com>
2026-05-29 12:15:01 +0545

    fuck this course

---
+++ pb.py
@@ -0,0 +1,11 @@
+#this program will ask for your name and age.
+print("Hello world")
+print("What is your name?")
+myName = input()
+print("its good to meet you," + myName)
+print("The length of your name is:")
+print(len(myName))
+print("what is your age ?")
+myAge =input()
+print()
+print('You will be ' + str(int(myAge)+1) +' in a year.')
