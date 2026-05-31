4550ded423b515ca74466ace2cc71ec8d5237d8c
Davitree <davidkhadka444@gmail.com>
2026-05-31 18:05:29 +0545

    Completed

---
+++ NameAndAge.py
@@ -0,0 +1,6 @@
+# print("What is your name?")
+name = input()
+# print("Which year were you born?")
+year = int(input())
+age =  2021 - year
+print(f"Hi {name}, you will be {age} years old at the end of the year 2021 ")
