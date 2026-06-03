d88a2622d8f972a7dc2436477f938a81b826b1fb
Davitree <davidkhadka444@gmail.com>
2026-06-03 07:38:57 +0545

    comppleted

---
+++ AbsoluteNumber.py
@@ -0,0 +1,7 @@
+num = int(input("Please type in a number:"))
+if num < 0:
+  num = num * (-1)
+  print("The absolute value of this number is ",num)
+else:
+
+  print("The absolute value of this number is ",num)
