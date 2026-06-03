7b78eecf07dd04755cf1e4ca068bbaa50cc8c934
Davitree <davidkhadka444@gmail.com>
2026-06-03 07:48:25 +0545

    completed

---
+++ Soup.py
@@ -0,0 +1,7 @@
+name = input("Please tell me your name:")
+if name == "Jerry":
+  print("Next please!")
+else:
+  soupno = int(input("How many portions of soup?"))
+  print("The total cost is",soupno*5.90)
+  print("Next please!")
