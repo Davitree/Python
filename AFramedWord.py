6662dbd3d31257dfa8543a7f07a1779ae0689a7f
Davitree <davidkhadka444@gmail.com>
2026-09-19 17:05:46 +0545

    cheated but completed

--- AFramedWord.py
+++ AFramedWord.py
@@ -1,13 +1,9 @@
 string = input("Word:")
 n = len(string)
-gaps = (28 - n) /2
+gaps = (28 - n) //2
 gap = int(gaps)
 
-if n % 2 == 0:
-  print("*" * 30)
-  print("*" + " "*gap+string+" "*gap+"*")
-  print("*" * 30)
-else:
-  print("*" * 30)
-  print("*" + " "*(gap-1)+string+" "*gap+"*")
-  print("*" * 30)
+
+print("*" * 30)
+print("*" + " "*gap+string+" "*(28-n-gaps)+"*")
+print("*" * 30)
